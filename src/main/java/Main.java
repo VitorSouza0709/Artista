e662fa2162691dc9e063413999bcd3d19727f1ff
VitorSouza0709 <169068391+VitorSouza0709@users.noreply.github.com>
2024-05-13 15:00:36 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+  public static void main(String[] args) { 
+    System.out.println("MC IG");
+    System.out.println("");
+    System.out.println("Todo mundo odeia o IG");
+    System.out.println("");
+    System.out.println("O título do álbum alude à série norte-americana de TV Todo mundo odeia o Chris, cujo nome também serviu de inspiração para o funkeiro fluminense MC Kevin O Chris batizar o disco Todo mundo ama o Chris ");
+    System.out.println("");
+    System.out.println("Lançado em 21 de março de 2024");
+    System.out.println("");
+    System.out.println("");
+    System.out.println("Músicas:");
+    System.out.println("");
+    System.out.println("Diz ai qual e o plano ? 12:17 ");
+    System.out.println("Só Mais um Copo 2:51");
+    System.out.println("É isso bebê ta certa! 4:32");
+      System.out.println("GoodNight MENINA 3 3:28");
+      System.out.println("Óla Moça 2:45");
+      System.out.println("Deus, Por Favor 4:43");
+      System.out.println("TMOIG 2:58");
+    
+  }
+}
